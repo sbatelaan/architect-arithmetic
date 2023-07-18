52214232de8a03920056f28955a11ee0e51b37a1
stephen Batelaan <sbatelaan91@gmail.com>
2023-07-17 20:30:54 -0700

    updated

--- Program.cs
+++ Program.cs
@@ -6,14 +6,24 @@ namespace ArchitectArithmetic
   {
     public static void Main(string[] args)
     {
-       double rectArea = Rect(4, 5);
-       Console.WriteLine($"Rectabgle area: {rectArea}");
 
-        double circleArea = Circle(4);
+        double materialCost = 180;
+
+       double rectArea = Rect(125500, 00);
+       Console.WriteLine($"Rectangle area: {rectArea}");
+
+        double circleArea = Circle(375);
         Console.WriteLine($"Circle area is about {circleArea}");
 
-        double triangleArea = Triangle(10, 9);
+        double triangleArea = Triangle(750, 500);
         Console.WriteLine($"Triangle has an area of: {triangleArea}");
+
+        double totalArea = rectArea + circleArea / 2 + triangleArea;
+        Console.WriteLine($"Total Area is: {totalArea}");
+
+        double totalCost = materialCost * totalArea;
+        // double roundedTotalCost = Math.Floor(totalCost * 100) / 100;
+        Console.WriteLine($"Total Cost of this job is: {roundedTotalCost} Pesos");
 
     }
 
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # architect-arithmetic
+
