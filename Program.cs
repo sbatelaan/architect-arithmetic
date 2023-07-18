3caf4e981246974c5121a4e5ff96b690c6fc5b59
stephen Batelaan <sbatelaan91@gmail.com>
2023-07-17 20:33:37 -0700

    formatted

--- Program.cs
+++ Program.cs
@@ -2,45 +2,48 @@ using System;
 
 namespace ArchitectArithmetic
 {
-  class Program
-  {
-    public static void Main(string[] args)
+    class Program
     {
+        public static void Main(string[] args)
+        {
 
-        double materialCost = 180;
+            double materialCost = 180;
 
-       double rectArea = Rect(125500, 00);
-       Console.WriteLine($"Rectangle area: {rectArea}");
+            double rectArea = Rect(125500, 00);
+            Console.WriteLine($"Rectangle area: {rectArea}");
 
-        double circleArea = Circle(375);
-        Console.WriteLine($"Circle area is about {circleArea}");
+            double circleArea = Circle(375);
+            Console.WriteLine($"Circle area is about {circleArea}");
 
-        double triangleArea = Triangle(750, 500);
-        Console.WriteLine($"Triangle has an area of: {triangleArea}");
+            double triangleArea = Triangle(750, 500);
+            Console.WriteLine($"Triangle has an area of: {triangleArea}");
 
-        double totalArea = rectArea + circleArea / 2 + triangleArea;
-        Console.WriteLine($"Total Area is: {totalArea}");
+            double totalArea = rectArea + circleArea / 2 + triangleArea;
+            Console.WriteLine($"Total Area is: {totalArea}");
 
-        double totalCost = materialCost * totalArea;
-        // double roundedTotalCost = Math.Floor(totalCost * 100) / 100;
-        Console.WriteLine($"Total Cost of this job is: {roundedTotalCost} Pesos");
+            double totalCost = materialCost * totalArea;
+            // double roundedTotalCost = Math.Floor(totalCost * 100) / 100;
+            Console.WriteLine($"Total Cost of this job is: {roundedTotalCost} Pesos");
 
-    }
+        }
 
-    static double Rect(double length, double width) {
+        static double Rect(double length, double width)
+        {
             double area = length * width;
             return area;
-    }
+        }
 
-    static double Circle(double radius) {
+        static double Circle(double radius)
+        {
             double area = Math.PI * Math.Pow(radius, 2);
             return area;
-    }
+        }
 
-    static double Triangle(double bottom, double height) {
-        double area = 0.5 * bottom * height;
-        return area;
-    }
+        static double Triangle(double bottom, double height)
+        {
+            double area = 0.5 * bottom * height;
+            return area;
+        }
 
-  }
+    }
 }
